de70d95606a1b4ab3da7a331fd86bd2dcb324b40
stefangeorgievw <stefangeorgievw@abv.bg>
2021-06-23 14:33:02 +0300

    Added new migration for emergency fund

--- SwartWallet.Infrastructure.Data/Migrations/20210623113047_AddedEmergencyFundProperties.Designer.cs
+++ SwartWallet.Infrastructure.Data/Migrations/20210623113047_AddedEmergencyFundProperties.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SwartWallet.Infrastructure.Data.Context;
 
 namespace SwartWallet.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(SmartWalletDbContext))]
-    partial class SmartWalletDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210623113047_AddedEmergencyFundProperties")]
+    partial class AddedEmergencyFundProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SwartWallet.Infrastructure.Data/Migrations/20210623113047_AddedEmergencyFundProperties.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SwartWallet.Infrastructure.Data.Migrations
+{
+    public partial class AddedEmergencyFundProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "CurrentEmergencyFund",
+                table: "Accounts",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ExpentedEmergencyFund",
+                table: "Accounts",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentEmergencyFund",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "ExpentedEmergencyFund",
+                table: "Accounts");
+        }
+    }
+}
