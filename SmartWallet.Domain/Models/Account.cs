c6050de93fa5b6467d7b4a2b7af20f5b0219d93c
stefangeorgievw <stefangeorgievw@abv.bg>
2021-06-23 14:32:39 +0300

    Added emergency fund properties

--- SmartWallet.Domain/Models/Account.cs
+++ SmartWallet.Domain/Models/Account.cs
@@ -7,5 +7,9 @@ namespace SmartWallet.Domain.Models
         public int Id { get; set; }
 
         public ICollection<MonthlyBudget> MonthlyBudgets { get; set; }
+
+        public decimal CurrentEmergencyFund { get; set; }
+
+        public decimal ExpentedEmergencyFund { get; set; }
     }
 }
